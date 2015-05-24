2946f840292cf394133276fc7fc9e82e4fb10fdb
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2015-05-24 09:39:29 +0800

    Initial Commit

---
+++ contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  contacts
+//
+//  Created by lingminjun on 15/5/24.
+//  Copyright (c) 2015年 shield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contacts
+//
+//  Created by lingminjun on 15/5/24.
+//  Copyright (c) 2015年 shield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
