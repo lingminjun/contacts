f43ccd1f94b289c98694c9f1cfb0625d69f151fa
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2015-05-27 00:27:03 +0800

    welcome基本实现

--- contacts/pages/welcome/CNWelcomeViewController.m
+++ contacts/pages/welcome/CNWelcomeViewController.m
@@ -8,39 +8,75 @@
 
 #import "CNWelcomeViewController.h"
 
-@interface CNWelcomeViewController ()
+#define cn_page_count (3)
+#define cn_dot_width  (14)
+
+@interface CNWelcomeViewController ()<SSNPageControlDelegate>
 
 @property (nonatomic,strong) NSString *url;
 @property (nonatomic,copy) NSDictionary *query;
 
-@property (nonatomic,strong) UIScrollView *panel;
+@property (nonatomic,strong) SSNPageControl *pageView;
+@property (nonatomic,strong) UIPageControl *dotView;
 
 @end
 
 @implementation CNWelcomeViewController
 
-- (UIScrollView *)panel {
-    if (_panel) {
-        return _panel;
+- (SSNPageControl *)pageView {
+    if (_pageView) {
+        return _pageView;
     }
     
-    _panel = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    _panel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-    _panel.scrollEnabled = YES;
-    return _panel;
+    _pageView = [[SSNPageControl alloc] initWithPageCount:cn_page_count];
+    _pageView.frame = self.view.bounds;
+    _pageView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    _pageView.delegate = self;
+    
+    for (NSInteger index = 0; index < cn_page_count; index++) {
+        NSString *imageName = [NSString stringWithFormat:@"welcome%02ld.jpg",(long)(index+1)];
+        UIImageView *imgV = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+        imgV.frame = _pageView.bounds;
+        imgV.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        [_pageView addView:imgV atIndex:index];
+    }
+    
+    return _pageView;
+}
+
+- (UIPageControl *)dotView {
+    if (_dotView) {
+        return _dotView;
+    }
+    
+    _dotView = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 0, (cn_page_count - 1)*cn_dot_width, 8)];
+    _dotView.numberOfPages = cn_page_count;
+    [_dotView sizeToFit];
+    _dotView.ssn_bottom = self.view.ssn_height - 20;
+    _dotView.ssn_center_x = self.view.ssn_center_x;
+    _dotView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin;
+    return _dotView;
+}
+
+- (UIButton *)skipButton {
+    UIButton *btn = [UIButton ssn_buttonWithSize:CGSizeMake(60, 40) font:[UIFont systemFontOfSize:14] color:[UIColor blackColor] selected:nil disabled:nil backgroud:nil selected:nil disabled:nil];
+    btn.ssn_center_y = 100;
+    btn.ssn_center_x = ssn_center([UIScreen mainScreen].bounds).x;
+    [btn ssn_addTarget:self touchAction:@selector(skipAction:)];
+    btn.ssn_normalTitleColor = [UIColor whiteColor];
+    btn.titleLabel.font = cn_normal_font;
+    btn.ssn_normalTitle = cn_localized(@"welcome.skip.button");
+    return btn;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
-    
-    UIButton *btn = [UIButton ssn_buttonWithSize:CGSizeMake(60, 40) font:[UIFont systemFontOfSize:14] color:[UIColor blackColor] selected:nil disabled:nil backgroud:nil selected:nil disabled:nil];
-    btn.ssn_center_y = 100;
-    btn.ssn_center_x = ssn_center([UIScreen mainScreen].bounds).x;
-    [btn ssn_addTarget:self touchAction:@selector(skipAction:)];
-    btn.ssn_normalTitle = @"跳过";
-    [self.view addSubview:btn];
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    [self.view addSubview:self.pageView];
+    [self.view addSubview:self.dotView];
+    [self.view addSubview:[self skipButton]];
 }
 
 - (void)skipAction:(id)sender {
@@ -61,6 +97,11 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark PageControlDelegate
+- (void)ssn_control:(SSNPageControl *)control didEnterPage:(UIView *)page atIndex:(NSUInteger)index {
+    self.dotView.currentPage = index;
+}
 
 #pragma mark - SSNPage
 - (BOOL)ssn_canRespondURL:(NSURL *)url query:(NSDictionary *)query {
