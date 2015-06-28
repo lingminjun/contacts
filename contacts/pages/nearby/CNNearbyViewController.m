680c9cec77e140630823202fe7df3ac4352cc266
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2015-06-28 20:48:25 +0800

    附近实现

--- contacts/pages/nearby/CNNearbyViewController.m
+++ contacts/pages/nearby/CNNearbyViewController.m
@@ -9,6 +9,7 @@
 #import "CNNearbyViewController.h"
 #import <BaiduMapAPI/BMapKit.h>
 #import "CNNearbyServer.h"
+#import "CNPerson.h"
 
 @interface CNNearbyViewController ()<BMKMapViewDelegate,UIGestureRecognizerDelegate>
 {
@@ -30,6 +31,16 @@
 @end
 
 @implementation CNNearbyViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        _nearbyPersons = [[NSMutableArray alloc] initWithCapacity:1];
+        _annotations = [[NSMutableArray alloc] initWithCapacity:1];
+    }
+    return self;
+}
 
 - (UIView *)bottomPanel {
     if (_bottomPanel) {
@@ -154,8 +165,6 @@
     self.tabBarItem.image = cn_image(@"icon_nearby_normal");
     self.tabBarItem.selectedImage = cn_image(@"icon_nearby_selected");
     
-    _nearbyPersons = [[NSMutableArray alloc] initWithCapacity:1];
-    
     
     _mapView = [[BMKMapView alloc] initWithFrame:self.view.bounds];
     _mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
@@ -246,8 +255,8 @@
         return ;
     }
     
-    NSString *mobile = self.selectdPerson.mobile;
-    NSString *msg = [NSString stringWithFormat:@"确定呼叫%@ %@吗？",self.selectdPerson.name,mobile];
+    NSString *mobile = [self.selectdPerson.mobile ssn_mobile344Format];
+    NSString *msg = [NSString stringWithFormat:@"确定呼叫%@:%@吗？",self.selectdPerson.name,mobile];
     
     [UIAlertView ssn_showConfirmationDialogWithTitle:@""
                                              message:msg
@@ -256,6 +265,9 @@
                                              handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
         NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
         if ([title isEqualToString:cn_localized(@"common.call.button")]) {
+            
+            //打点，呼叫次数
+            
             NSString *url = [NSString stringWithFormat:@"tel:%@",mobile];
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
         }
@@ -263,6 +275,93 @@
 }
 
 - (void)gpsAction:(id)sender {
+    
+    if (self.selectdPerson == nil) {
+        return ;
+    }
+    
+    NSString *msg = [NSString stringWithFormat:@"确定导航到%@%@的位置？",self.selectdPerson.name,(_selectdPerson.addressLabel == CNCompanyAddressLabel ?cn_localized(@"user.company.address.label") : cn_localized(@"user.home.address.label"))];
+    [UIAlertView ssn_showConfirmationDialogWithTitle:@""
+                                             message:msg
+                                              cancel:cn_localized(@"common.cancel.button")
+                                             confirm:cn_localized(@"common.submit.button")
+                                             handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
+                                                 NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+                                                 if ([title isEqualToString:cn_localized(@"common.submit.button")]) {
+                                                     
+                                                     //打点，导航次数
+                                                     
+                                                     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"baidumap://"]]) {
+                                                         [self nativeNavi];
+                                                     }
+                                                     else {
+                                                         [self webNavi];
+                                                     }
+                                                 }
+                                             }];
+}
+
+- (IBAction)nativeNavi
+{
+    //初始化调启导航时的参数管理类
+    BMKNaviPara* para = [[BMKNaviPara alloc]init];
+    //指定导航类型
+    para.naviType = BMK_NAVI_TYPE_NATIVE;
+    
+    //初始化终点节点
+    BMKPlanNode* end = [[BMKPlanNode alloc] init];
+    
+    //指定终点经纬度
+    CLLocationCoordinate2D coor2;
+    coor2.latitude = self.selectdPerson.latitude;
+    coor2.longitude = self.selectdPerson.longitude;
+    end.pt = coor2;
+    //指定终点名称
+    NSString *title = [NSString stringWithFormat:@"%@%@的位置",_selectdPerson.name,(_selectdPerson.addressLabel == CNCompanyAddressLabel ?cn_localized(@"user.company.address.label") : cn_localized(@"user.home.address.label"))];
+    end.name = title;
+    //指定终点
+    para.endPoint = end;
+    
+    //指定返回自定义scheme
+    para.appScheme = @"baidumapsdk://mapsdk.baidu.com";
+    
+    //调启百度地图客户端导航
+    [BMKNavigation openBaiduMapNavigation:para];
+}
+
+- (IBAction)webNavi
+{
+    //初始化调启导航时的参数管理类
+    BMKNaviPara* para = [[BMKNaviPara alloc]init];
+    //指定导航类型
+    para.naviType = BMK_NAVI_TYPE_WEB;
+    
+    //初始化起点节点
+    BMKPlanNode* start = [[BMKPlanNode alloc]init];
+    
+    //指定起点经纬度
+    CLLocationCoordinate2D coor1 = _here;
+    start.pt = coor1;
+    //指定起点名称
+    start.name = cn_localized(@"location.me.location");
+    //指定起点
+    para.startPoint = start;
+    
+    
+    //初始化终点节点
+    BMKPlanNode* end = [[BMKPlanNode alloc]init];
+    CLLocationCoordinate2D coor2;
+    coor2.latitude = self.selectdPerson.latitude;
+    coor2.longitude = self.selectdPerson.longitude;
+    end.pt = coor2;
+    para.endPoint = end;
+    //指定终点名称
+    NSString *title = [NSString stringWithFormat:@"%@%@的位置",_selectdPerson.name,(_selectdPerson.addressLabel == CNCompanyAddressLabel ?cn_localized(@"user.company.address.label") : cn_localized(@"user.home.address.label"))];
+    end.name = title;
+    //指定调启导航的app名称
+    para.appName = [SSNAppInfo appLocalizedName];
+    //调启web导航
+    [BMKNavigation openBaiduMapNavigation:para];
 }
 
 #pragma mark - Annotation
