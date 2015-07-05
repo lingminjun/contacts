e4b9fe2ed290a6b0219e1a229f620e3d0b42da02
Ling Minjun <lingminjun@lingminjundeMacBook-Pro.local>
2015-07-05 22:01:25 +0800

    附近cell修改

--- contacts/views/nearby/CNNearbyPersonCell.m
+++ contacts/views/nearby/CNNearbyPersonCell.m
@@ -15,86 +15,77 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        
-        CGRect frame = CGRectMake(0, 0, self.bounds.size.width, NEARBY_CELL_HEIGHT);
-        UIView * panel = [[UIView alloc] initWithFrame:frame];
-        panel.backgroundColor = [UIColor clearColor];
-        panel.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleTopMargin;
-        {
-            UIImageView *backgroud = [[UIImageView alloc] initWithFrame:panel.bounds];
-            backgroud.ssn_width = panel.ssn_width;
+            UIImageView *backgroud = [[UIImageView alloc] initWithFrame:self.contentView.bounds];
             backgroud.image = [[UIImage ssn_imageWithColor:cn_backgroud_white_color cornerRadius:2] ssn_centerStretchImage];
             backgroud.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-            backgroud.userInteractionEnabled = YES;
-            backgroud.center = ssn_center(panel.bounds);
-            ssn_panel_set(panel, backgroud, backgroud);
+            backgroud.center = ssn_center(self.contentView.bounds);
+            ssn_panel_set(self.contentView, backgroud, backgroud);
+        
+        
+        SSNUITableLayout *layout = [self.contentView ssn_tableLayoutWithRowCount:3 columnCount:1];
+        layout.contentInset = cn_panel_edge;
+        
+        //头部
+        UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, cn_screen_width - cn_left_edge_width - cn_right_edge_width, 30)];
+        {
+            int local_width = 100;
+            SSNUITableLayout *ly = [topView ssn_tableLayoutWithRowCount:1 columnCount:2];
+            [ly setColumnInfo:ssn_layout_table_column_v2(local_width) atColumn:1];
             
-            {
-                SSNUITableLayout *layout = [backgroud ssn_tableLayoutWithRowCount:3 columnCount:1];
-                layout.contentInset = cn_panel_edge;
-                
-                UIView *topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, backgroud.ssn_width - (cn_left_edge_width + cn_right_edge_width), 30)];
-                {
-                    int local_width = 100;
-                    SSNUITableLayout *ly = [topView ssn_tableLayoutWithRowCount:1 columnCount:2];
-                    [ly setColumnInfo:ssn_layout_table_column_v2(local_width) atColumn:1];
-                    
-                    _nameLabel = [UILabel ssn_labelWithWidth:topView.ssn_width - local_width
-                                                            font:cn_title_font
-                                                           color:cn_text_normal_color
-                                                       backgroud:[UIColor clearColor]
-                                                       alignment:NSTextAlignmentLeft
-                                                       multiLine:NO];
-                    ssn_layout_add_v2(ly, _nameLabel, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _nameLabel);
-                    
-                    _distanceLabel = [UILabel ssn_labelWithWidth:local_width
-                                                   font:cn_assist_font
-                                                  color:cn_text_assist_color
-                                              backgroud:[UIColor clearColor]
-                                              alignment:NSTextAlignmentRight
-                                              multiLine:NO];
-                    ssn_layout_add_v2(ly, _distanceLabel, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _distanceLabel);
-                }
-                ssn_layout_add_v2(layout, topView, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), topView);
-                
-                _addressLabel = [UILabel ssn_labelWithWidth:backgroud.ssn_width - (cn_left_edge_width + cn_right_edge_width)
-                                                        font:cn_normal_font
-                                                       color:cn_text_assist_color
-                                                   backgroud:[UIColor clearColor]
-                                                   alignment:NSTextAlignmentLeft
-                                                   multiLine:NO];
-                ssn_layout_add_v2(layout, _addressLabel, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _addressLabel);
-                
-                UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, backgroud.ssn_width - (cn_left_edge_width + cn_right_edge_width), 40)];
-                {
-                    SSNUITableLayout *ly = [bottomView ssn_tableLayoutWithRowCount:1 columnCount:3];
-                    [ly setColumnInfo:ssn_layout_table_column_v2(1) atColumn:1];
-                    
-                    UIButton *btn = [UIButton cn_transparent_button];
-                    btn.ssn_normalTitle = cn_localized(@"location.call.label");
-                    btn.ssn_normalImage = cn_image(@"icon_call");
-                    [btn ssn_addTarget:self touchAction:@selector(callAction:)];
-                    ssn_layout_add_v2(ly, btn, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), call);
-                    _callPhoneBtn = btn;
-                    
-                    UIImageView *line = [UIImageView ssn_lineWithWidth:30 color:cn_separator_line_color orientation:UIInterfaceOrientationLandscapeLeft];
-                    ssn_layout_add_v2(ly, line, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), line);
-                    
-                    btn = [UIButton cn_transparent_button];
-                    btn.ssn_normalTitle = cn_localized(@"location.gps.label");
-                    btn.ssn_normalImage = cn_image(@"icon_gps");
-                    [btn ssn_addTarget:self touchAction:@selector(gpsAction:)];
-                    
-                    ssn_layout_add_v2(ly, btn, 2, ssn_layout_table_cell_v2(SSNUIContentModeCenter), gps);
-                    _gpsBtn = btn;
-                }
-                
-                [layout setRowInfo:ssn_layout_table_row(bottomView.ssn_height) atRow:2];
-                ssn_layout_add_v2(layout, bottomView, 2, ssn_layout_table_cell_v2(SSNUIContentModeCenter), bottomView);
-            }
+            _nameLabel = [UILabel ssn_labelWithWidth:topView.ssn_width - local_width
+                                                font:cn_title_font
+                                               color:cn_text_normal_color
+                                           backgroud:[UIColor clearColor]
+                                           alignment:NSTextAlignmentLeft
+                                           multiLine:NO];
+            ssn_layout_add_v2(ly, _nameLabel, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _nameLabel);
+            
+            _distanceLabel = [UILabel ssn_labelWithWidth:local_width
+                                                    font:cn_assist_font
+                                                   color:cn_text_assist_color
+                                               backgroud:[UIColor clearColor]
+                                               alignment:NSTextAlignmentRight
+                                               multiLine:NO];
+            ssn_layout_add_v2(ly, _distanceLabel, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _distanceLabel);
+        }
+        ssn_layout_add_v2(layout, topView, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), topView);
+        
+        //内容
+        _addressLabel = [UILabel ssn_labelWithWidth:cn_screen_width - (cn_left_edge_width + cn_right_edge_width)
+                                               font:cn_normal_font
+                                              color:cn_text_assist_color
+                                          backgroud:[UIColor redColor]
+                                          alignment:NSTextAlignmentLeft
+                                          multiLine:NO];
+        ssn_layout_add_v2(layout, _addressLabel, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), _addressLabel);
+        
+        //底部
+        UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, cn_screen_width - cn_left_edge_width - cn_right_edge_width, 40)];
+        {
+            SSNUITableLayout *ly = [bottomView ssn_tableLayoutWithRowCount:1 columnCount:3];
+            [ly setColumnInfo:ssn_layout_table_column_v2(40) atColumn:1];
+            
+            UIButton *btn = [UIButton cn_transparent_button];
+            btn.ssn_normalTitle = cn_localized(@"location.call.label");
+            btn.ssn_normalImage = cn_image(@"icon_call");
+            [btn ssn_addTarget:self touchAction:@selector(callAction:)];
+            ssn_layout_add_v2(ly, btn, 0, ssn_layout_table_cell_v2(SSNUIContentModeCenter), call);
+            _callPhoneBtn = btn;
+            
+            UIImageView *line = [UIImageView ssn_lineWithWidth:30 color:cn_separator_line_color orientation:UIInterfaceOrientationLandscapeLeft];
+            ssn_layout_add_v2(ly, line, 1, ssn_layout_table_cell_v2(SSNUIContentModeCenter), line);
+            
+            btn = [UIButton cn_transparent_button];
+            btn.ssn_normalTitle = cn_localized(@"location.gps.label");
+            btn.ssn_normalImage = cn_image(@"icon_gps");
+            [btn ssn_addTarget:self touchAction:@selector(gpsAction:)];
+            
+            ssn_layout_add_v2(ly, btn, 2, ssn_layout_table_cell_v2(SSNUIContentModeCenter), gps);
+            _gpsBtn = btn;
         }
         
-        [self.contentView addSubview:panel];
+        [layout setRowInfo:ssn_layout_table_row(bottomView.ssn_height) atRow:2];
+        ssn_layout_add_v2(layout, bottomView, 2, ssn_layout_table_cell_v2(SSNUIContentModeCenter), bottomView);
     }
     return self;
 }
